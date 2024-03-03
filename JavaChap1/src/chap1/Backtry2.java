238ece3eb29552e3fd7af851f9e76c9c95a22860
Seungmin Han <als5653a@gmail.com>
2024-03-03 20:11:32 +0900

    2024-03-03

---
+++ JavaChap1/src/chap1/Backtry2.java
@@ -0,0 +1,30 @@
+package chap1;
+import java.util.Scanner;
+
+public class Backtry2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr1 = new int[10];
+		int count = 0;
+		int temp = 0;
+		
+		for(int i=0; i<10; i++) {
+			arr1[i] = sc.nextInt() % 42;
+		}
+		for(int i=0; i<10; i++) {
+			temp = 0;
+			for(int j=i+1; j<10; j++) {
+				if(arr1[i] == arr1[j]) {
+					temp++;
+				}
+			}
+			if(temp == 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+	}
+
+}
