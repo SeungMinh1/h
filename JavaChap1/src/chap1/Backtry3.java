7eb38131fac23e1935a424066a598daf5cbb7364
Seungmin Han <als5653a@gmail.com>
2024-03-04 23:05:15 +0900

    2024/03/04

---
+++ JavaChap1/src/chap1/Backtry3.java
@@ -0,0 +1,38 @@
+package chap1;
+
+import java.util.Scanner;
+
+public class Backtry3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); //바구니
+		int m = sc.nextInt(); // 횟수
+		
+		
+		int[] basket = new int[n+1];
+		for(int k=1; k<=n; k++) {
+			basket[k] = k;
+		}
+		
+		for(int k=0; k<m; k++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+			for(int h=i; h<=j; h++) {
+				int box = 0; //저장할 공간
+				
+				box = basket[h];
+				basket[h] = basket[j];
+				basket[j] = box;
+				--j;
+			}
+		}
+		
+		for(int q=1; q<=n; q++) {
+			System.out.print(basket[q]+" ");
+		}
+
+	}
+
+}
